2943d7a0e93aaeaaea8108d7f29e73032709635d
lty88 <lty8oo@163.com>
2019-12-31 10:09:45 +0800

    product_page

--- build/entry.js
+++ build/entry.js
@@ -3,5 +3,6 @@ module.exports = {
     main: './src/js/index.js',
     login: './src/js/login.js',
     register: './src/js/register.js',
+    Product_Page:'./src/js/Product_Page.js'
 }
 
--- build/html.config.js
+++ build/html.config.js
@@ -56,4 +56,22 @@ module.exports = [
             removeEmptyAttributes: true
         }
     }),
+     // —> Product_page
+     new HtmlWebpackPlugin({
+        // 模板文件
+        template: "./src/pages/Product_Page.html",
+        // 文件名(相对于output.path)，可通过文件名设置目录，如 static/pages/detail.html
+        filename: "static/pages/Product_Page.html",
+        // 静态资源位置
+        inject: "body",
+        // 指定输出文件所依赖的入口文件（*.js）的[name]
+        chunks: ["Product_Page"],
+        // 控制压缩
+        minify: {
+            collapseWhitespace: false,
+            removeComments: true,
+            removeAttributeQuotes: true,
+            removeEmptyAttributes: true
+        }
+    }),
 ]
