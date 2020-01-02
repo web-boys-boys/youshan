7ddce8190a68b2ddb13fb58895ca5e48289fedc4
lty88 <lty8oo@163.com>
2020-01-02 18:29:12 +0800

    numpages

--- src/js/Product_Page.js
+++ src/js/Product_Page.js
@@ -1,14 +1,8 @@
 import '../less/index.less';
 import '../less/util.less';
 import '../less/Product_Page.less';
+//选择菜单
 $('.fr_menu li').click(function(){
-    $(this).find('a').css({color:'orange'})
-}, 
-function(){
-    $(this).find('a').css({color:'blue'})
-}
-).click(function(){
-    $(this).css({letterSpacing:"3px",fontWeight:"bold"}).siblings().css({letterSpacing:"0px",fontWeight:"normal"})
-}).first().trigger("click");
-
-
+    $(this).children('a').addClass('active').parent().siblings().children('a').removeClass('active');
+  
+})
