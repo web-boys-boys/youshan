8e6c3d42ca11a4fde5dfb06cb0e92dc0de204bf1
lty88 <lty8oo@163.com>
2020-01-03 11:18:40 +0800

    mod htmljs

--- src/js/Menu_Page.js
+++ src/js/Menu_Page.js
@@ -1,20 +1,7 @@
 import '../less/index.less';
 import '../less/util.less';
 import '../less/Menu_Page.less';
-<<<<<<< HEAD
-//一级菜单
-$('.fr_menu li').click(function(){
-    $(this).find('a').css({color:'orange'})
-}, 
-function(){
-    $(this).find('a').css({color:'blue'})
-}
-).click(function(){
-    $(this).css({letterSpacing:"3px",fontWeight:"bold"}).siblings().css({letterSpacing:"0px",fontWeight:"normal"})
-}).first().trigger("click");
-//评星
-
-=======
+import './JsHtml.js';
 
 //选择菜单
 $('.fr_menu li').click(function(){
@@ -24,52 +11,4 @@ $('.fr_menu li').click(function(){
     console.log(idx);
     $(this).eq(idx).addClass('show').siblings('.list').removeClass('hidd')
 })
-//评星
-$('.star')(function(){
-    var aSpan=document.getElementsByClassName(".star")[0];
-    var aStxt=document.getElementsByClassName(".star-txt")[0];
-    var aBstar=aSpan.getElementsByTagName("span");
-    var arrBtxt=["很差","较差","还行","推荐","力荐"];
-    var num=0;
-    let onOff=true;
-    for(var i= 0;i<aBstar.length;i++){
-            aBstar[i].index=i;
-            //鼠标移入
-            aBstar[i].onmouseover=function(){
-                if(onOff) {
-                    num = this.index;
-                    aStxt.innerHTML = arrBtxt[num];
-                    for (var i = 0; i <=this.index; i++) {
-                        aBstar[i].style.backgroundPosition = "0 0";
-                    }
-                }
-            };
-            //鼠标移开
-            aBstar[i].onmouseout=function(){
-             if(onOff){//设定个开关，等开关为真就执行鼠标移除的代码
-                 aStxt.innerHTML="";
-                 for(var i=0;i<=this.index;i++){
-                     aBstar[i].style.backgroundPosition="-39px 0";
-                 }
-               }
-             };
-            //鼠标点击
-            aBstar[i].onclick=function(){
-                onOff=false;//开关为假就不执行鼠标移除的代码
-                //先清空
-                aStxt.innerHTML="";
-                for(var i=0;i<aBstar.length;i++){
-                    aBstar[i].style.backgroundPosition="-39px 0";
-                }
-                //点击当前星星，之前的都点亮包含自己
-                num = this.index ;
-                aStxt.innerHTML=arrBtxt[num];
-                for(var i=0;i<=this.index;i++){
-                    aBstar[i].style.backgroundPosition="0 0";
 
-                }
-
-            };
-        }
-})
->>>>>>> 003076587bae703e837b70e5f87f175c9175176c
--- src/js/Product_Page.js
+++ src/js/Product_Page.js
@@ -1,22 +1,9 @@
 import '../less/index.less';
 import '../less/util.less';
 import '../less/Product_Page.less';
-<<<<<<< HEAD
-$('.fr_menu li').click(function(){
-    $(this).find('a').css({color:'orange'})
-}, 
-function(){
-    $(this).find('a').css({color:'blue'})
-}
-).click(function(){
-    $(this).css({letterSpacing:"3px",fontWeight:"bold"}).siblings().css({letterSpacing:"0px",fontWeight:"normal"})
-}).first().trigger("click");
-
-
-=======
+import './JsHtml.js';
 //选择菜单
 $('.fr_menu li').click(function(){
     $(this).children('a').addClass('active').parent().siblings().children('a').removeClass('active');
   
 })
->>>>>>> 003076587bae703e837b70e5f87f175c9175176c
