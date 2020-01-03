8bb269daba5c0c0e867c46dc053646c8b48a5d8e
LIWANG1998 <535287382@QQ.COM>
2020-01-03 11:54:44 -0800

    jshtml1.3

--- src/js/JsHtml.js
+++ src/js/JsHtml.js
@@ -10,7 +10,7 @@ function headerHtml() {
             </select>
             <span class="welcome_text">你好，欢迎来到友膳</span>
         </div>
-        <div class="header_logo"><a href="index.html"><img src="${require('../images/YOUSHAN.svg')}" alt=""></a></div>
+        <div class="header_logo"><a href="../../index.html"><img src="${require('../images/YOUSHAN.svg')}" alt=""></a></div>
         <div class="header_right flex-between-b">
             <section class="flex-between-b">
                 <span class="login_rigster_img"><img src="${require('../images/login_register.png')}" alt="" sizes=""
@@ -230,3 +230,50 @@ let htmlone=`<div class="banner">
 $(".main").prepend(htmlone);
 }
 headerHtml();
+
+import Swiper from "swiper";
+import "swiper/css/swiper.css";
+// 轮播图
+let mySwiper = new Swiper('.swiper-container', {
+    loop: true, // 循环模式选项
+    // 自动播放
+    autoplay: true,
+    // 切换效果
+    effect: 'coverflow',
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+})
+
+$(".swiper_img_click").on("click", function () {
+    console.log($(this).attr("src"))
+})
+
+
+console.log($(".menu_grop"));
+$(".menu_grop").each((index, itme) => {
+    console.log(index);
+    $(itme).data("id", index);
+    // $(".menu_grop_s").css({ opacity: "1" });
+})
+
+$(".menu_grop").mouseenter(function () {
+    $(".menu_bottom").css({ opacity: "1", zIndex: "10" });
+    // $(".menu_grop_s").css({ opacity: "0" });
+    // let index = $(this).data("id");
+    // $($(".menu_grop_s")[index]).css({ opacity: "1" });
+    //    $(".menu_grop_s").html($(this).data("id"));
+})
+$(".menu_bar").mouseleave(function () {
+    $(".menu_bottom").css({ opacity: "0", zIndex: "-10" });
+})
+
+
+
+
--- src/js/Menu_Page.js
+++ src/js/Menu_Page.js
@@ -3,11 +3,14 @@ import '../less/util.less';
 import '../less/Menu_Page.less';
 import './JsHtml.js';
 
+import Swiper from "swiper";
+import "swiper/css/swiper.css";
+
 //选择菜单
-$('.fr_menu li').click(function(){
+$('.fr_menu li').click(function () {
     $(this).children('a').addClass('active').parent().siblings().children('a').removeClass('active');
     //获取鼠标当前移入这个li标签的索引
-    let idx=$(this).index()
+    let idx = $(this).index()
     console.log(idx);
     $(this).eq(idx).addClass('show').siblings('.list').removeClass('hidd')
 })
