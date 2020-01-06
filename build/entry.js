8ee13a0186ce88a0fc2d4f9862558876bad62f36
e <984136543@qq.com>
2020-01-06 11:22:35 +0800

    ycy1.6

--- build/entry.js
+++ build/entry.js
@@ -1,10 +1,10 @@
 // entry module
 module.exports = {
-    main: './src/js/index.js',
-    login: './src/js/login.js',
-    register: './src/js/register.js',
-    Product_Page:'./src/js/Product_Page.js',
-    Menu_Page:'./src/js/Menu_Page.js',
-    orderPages: './src/js/order-pages.js',
-}
-
+  main: "./src/js/index.js",
+  login: "./src/js/login.js",
+  register: "./src/js/register.js",
+  Product_Page: "./src/js/Product_Page.js",
+  Menu_Page: "./src/js/Menu_Page.js",
+  orderPages: "./src/js/order-pages.js",
+  commodity: "./src/js/commodity.js",
+};
