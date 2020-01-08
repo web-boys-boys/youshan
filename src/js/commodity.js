5d4148e89e72b43692d5b8ca7dfccd3b7f38cc78
e <984136543@qq.com>
2020-01-08 12:55:08 +0800

    lty1.2

--- src/js/commodity.js
+++ src/js/commodity.js
@@ -9,6 +9,11 @@ fun_footer();
 
 import "../less/commodity.less";
 
+// bootstrap
+import "popper.js";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.css";
+
 //点击增加按钮
 $(".jia").each(function() {
   $(this).on("click", function() {
@@ -57,3 +62,34 @@ $('.shopping-buy-left').on('click',function(){
 $('.shopping-buy-right').on('click',function(){
     // location.href = ' ';
 })
+
+
+// 点击商品详情和用户信息
+$(".introduction-onclick").on("click", function() {
+  // console.log($(this));
+  $(this)
+    .addClass("get-ellipse")
+    .siblings()
+    .removeClass("get-ellipse");
+});
+// console.log($(".tab-choice-head").children(), 123);
+//点击更换页面
+$.each($(".tab-choice-head").children(), (index, item) => {
+
+  $(item).data("index", index);
+});
+
+$(".tab-choice-head")
+  .children()
+  .click(function() {
+    let ch_index = $(this).data("index");
+    $.each($(".tab-main-container").children(), (index, item) => {
+      if (index == ch_index) {
+        $(item)
+          .show()
+          .siblings()
+          .hide();
+      }
+    });
+  });
+//
